1af2b086c5e63d8b7aec0c51826ca8045f4ec0c8
MominHussain123 <hafizmominhussain222@gmail.com>
2025-01-01 02:51:35 +0500

    Updated Code

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
 require("dotenv").config();
 
-app.use(express.static(path.resolve(path.join(__dirname,"public"))));
+app.use(express.static(path.resolve(path.join(__dirname, "public"))));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(body_parser.json());
-app.use(routs); 
+app.use(routs);
 
-app.use(cors({
-    origin:"https://user-info-4jzj27k9u-hafizmominhussain222-gmailcoms-projects.vercel.app",
-    withCredentials:true,
-}))
 const db = process.env.MONGODB_URL;
 mongoose.connect(db).then(() => {
     console.log("Connected");
@@ -23,7 +23,7 @@ mongoose.connect(db).then(() => {
     console.log("Not connect " + err);
 });
 const port = process.env.PORT;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server is riunning on PORT", port);
 });
 
